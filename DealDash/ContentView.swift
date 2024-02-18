365e97cd53b576e9bfa32fe3f14c0c7d2d71d4fb
Daniel Waiguru <danielirungu91@gmail.com>
2024-02-18 17:18:11 +0300

    initial commit

---
+++ DealDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DealDash
+//
+//  Created by Daniel Waiguru on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DealDash/DealDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealDashApp.swift
+//  DealDash
+//
+//  Created by Daniel Waiguru on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DealDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
